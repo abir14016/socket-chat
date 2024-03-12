baa9f5abace2a7c71be8a439ff812d1772f8a40e
abir14016 <abir14016@gmail.com>
2024-03-12 14:30:23 +0600

    connection and disconnect event in server

--- server/index.js
+++ server/index.js
@@ -1,14 +1,35 @@
-const express = require("express");
+import express from "express";
 const app = express();
-const http = require("http");
+import { createServer } from "http";
+
+const { Server } = require("socket.io");
+
 require('dotenv').config()
 const port = process.env.PORT || 3001;
 
 //using cors middleware to avoid conflicts between server and client
-const cors = require("cors");
+import cors from "cors";
 app.use(cors());
 
-const server = http.createServer(app);
+
+
+const server = createServer(app);
+
+// new instance of Server with solving cors issues
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"]
+    }
+});
+
+io.on("connection", (socket) => {
+    console.log(socket.id);
+
+    socket.on("disconnect", () => {
+        console.log("User disconnected", socket.id);
+    })
+})
 
 
 server.listen(3001, () => {
