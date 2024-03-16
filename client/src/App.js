7a98c637efe0e15927d8a787cda3e31a3cdc268e
abir14016 <abir14016@gmail.com>
2024-03-17 00:51:41 +0600

    loop through the messages and displayed

--- client/src/App.js
+++ client/src/App.js
@@ -10,75 +10,80 @@ function App() {
   const { register, formState: { errors }, handleSubmit, reset } = useForm();
   const [userName, setUserName] = useState(""); // State for userName
   const [room, setRoom] = useState(""); // State for room
+  const [showChat, setShowChat] = useState(false);
 
   const onSubmit = (data) => {
     socket.emit("join_room", data.room);
     setUserName(data.userName); // Set userName state
     setRoom(data.room); // Set room state
     reset();
+    setShowChat(true);
   }
 
   return (
     <div className="App">
-      <div className="flex justify-center items-center h-screen">
-        <div className="card w-96 bg-neutral text-primary-content">
-          <div className="card-body">
-            <h2 className="card-title">Join a chat!</h2>
+      {!showChat ?
+        (<div className="flex justify-center items-center h-screen">
+          <div className="card w-96 bg-neutral text-primary-content">
+            <div className="card-body">
+              <h2 className="card-title">Join a chat!</h2>
 
-            <form onSubmit={handleSubmit(onSubmit)}>
-              {/* userName field */}
-              <div className="form-control w-full max-w-xs">
-                <label className="label">
-                  <span className="label-text text-white">Enter Your Name</span>
-                </label>
-                <input
-                  type="text"
-                  placeholder="Your Name..."
-                  className="text-black input input-bordered w-full max-w-xs"
-                  {...register("userName", {
-                    required: {
-                      value: true,
-                      message: 'you must provide your name'
-                    }
-                  })}
-                />
-                <label className="label">
-                  {errors.userName?.type === 'required' && <span className="label-text-alt text-error">{errors.userName.message}</span>}
-                </label>
-              </div>
-              {/* userName field */}
+              <form onSubmit={handleSubmit(onSubmit)}>
+                {/* userName field */}
+                <div className="form-control w-full max-w-xs">
+                  <label className="label">
+                    <span className="label-text text-white">Enter Your Name</span>
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="Your Name..."
+                    className="text-black input input-bordered w-full max-w-xs"
+                    {...register("userName", {
+                      required: {
+                        value: true,
+                        message: 'you must provide your name'
+                      }
+                    })}
+                  />
+                  <label className="label">
+                    {errors.userName?.type === 'required' && <span className="label-text-alt text-error">{errors.userName.message}</span>}
+                  </label>
+                </div>
+                {/* userName field */}
 
-              {/* room field */}
-              <div className="form-control w-full max-w-xs">
-                <label className="label">
-                  <span className="label-text text-white">Enter Room ID</span>
-                </label>
-                <input
-                  type="text"
-                  placeholder="Room ID..."
-                  className="text-black input input-bordered w-full max-w-xs"
-                  {...register("room", {
-                    required: {
-                      value: true,
-                      message: 'you must provide a room'
-                    }
-                  })}
-                />
-                <label className="label">
-                  {errors.room?.type === 'required' && <span className="label-text-alt text-error">{errors.room.message}</span>}
-                </label>
-              </div>
-              {/* room field */}
+                {/* room field */}
+                <div className="form-control w-full max-w-xs">
+                  <label className="label">
+                    <span className="label-text text-white">Enter Room ID</span>
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="Room ID..."
+                    className="text-black input input-bordered w-full max-w-xs"
+                    {...register("room", {
+                      required: {
+                        value: true,
+                        message: 'you must provide a room'
+                      }
+                    })}
+                  />
+                  <label className="label">
+                    {errors.room?.type === 'required' && <span className="label-text-alt text-error">{errors.room.message}</span>}
+                  </label>
+                </div>
+                {/* room field */}
 
-              <div className="card-actions justify-end">
-                <input className="btn btn-outline btn-secondary" type="submit" value="Join a room" />
-              </div>
-            </form>
+                <div className="card-actions justify-end">
+                  <input className="btn btn-outline btn-secondary" type="submit" value="Join a room" />
+                </div>
+              </form>
 
+            </div>
           </div>
-        </div>
-      </div>
-      <Chat socket={socket} userName={userName} room={room} />
+        </div>)
+        :
+        (<Chat socket={socket} userName={userName} room={room} />)
+      }
     </div>
   );
 }
--- client/src/Chat.js
+++ client/src/Chat.js
@@ -3,6 +3,8 @@ import { useState } from 'react';
 
 const Chat = ({ socket, userName, room }) => {
     const [currentMessage, setCurrentMessage] = useState("");
+    const [messageList, setMessageList] = useState([]);
+
     const sendMessage = async () => {
         if (currentMessage !== "") {
             const messageData = {
@@ -19,26 +21,32 @@ const Chat = ({ socket, userName, room }) => {
     useEffect(() => {
         socket.on("receive_message", (data) => {
             //listening event that emitted from backend
-            console.log(data);
+            setMessageList((list) => [...list, data]);
         })
     }, [socket])
 
     return (
-        <div className="card w-96 bg-primary text-primary-content">
-            <div className="card-body">
-                <h2 className="card-title">Live Chat</h2>
-                <p>If a dog chews shoes whose shoes does he choose?</p>
-                <div className="card-actions justify-center">
-                    <label className="input input-bordered flex justify-between items-center gap-2">
-                        <input
-                            onChange={(event) => { setCurrentMessage(event.target.value) }}
-                            type="text"
-                            className="grow text-black"
-                            placeholder="Type Here..." />
-                        <button
-                            onClick={sendMessage}
-                            className="btn">&#9658;</button>
-                    </label>
+        <div className="flex justify-center items-center h-screen">
+            <div className="card w-96 bg-primary text-primary-content">
+                <div className="card-body">
+                    <h2 className="card-title">Live Chat</h2>
+                    {
+                        messageList.map((messageContent) => {
+                            return <p>{messageContent.message}</p>
+                        })
+                    }
+                    <div className="card-actions justify-center">
+                        <label className="input input-bordered flex justify-between items-center gap-2">
+                            <input
+                                onChange={(event) => { setCurrentMessage(event.target.value) }}
+                                type="text"
+                                className="grow text-black"
+                                placeholder="Type Here..." />
+                            <button
+                                onClick={sendMessage}
+                                className="btn">&#9658;</button>
+                        </label>
+                    </div>
                 </div>
             </div>
         </div>
